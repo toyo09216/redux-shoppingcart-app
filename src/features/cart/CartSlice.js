c1ac8c13d6a95e8b4364cce74dd59f5a7c138f15
toyo09216 <atsushi09216@gmail.com>
2025-01-28 14:42:12 +0900

    [add] 全ての商品を削除するactionとreducerを作成

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -11,7 +11,14 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
 })
 
+console.log(cartSlice);
+
+export const { clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
