ba5cb06f7a616156134fbc2876a3724f6a825a15
toyo09216 <atsushi09216@gmail.com>
2025-01-28 11:45:56 +0900

    [add] 買い物かごに、何も入っていない場合の表示

---
+++ src/components/CartContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+const CartContainer = () => {
+  const { amount } = useSelector((store) => store.cart);
+  if (amount < 1) {
+    return (
+      <section className='cart'>
+        <header>
+          <h2>買い物かご</h2>
+          <h4 className='empty-cart'>何も入っていません。。。😹</h4>
+        </header>
+      </section>
+    )
+  }
+}
+
+export default CartContainer
