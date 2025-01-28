55997b0a55742e946d9bf024aba0a3624935c318
toyo09216 <atsushi09216@gmail.com>
2025-01-28 17:23:02 +0900

    [add] 合計金額、商品数がカートの状態が変わるごとに発火するようなreducerを作成

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
+import { useEffect } from 'react';
+import { calculateTotals } from './features/cart/CartSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
