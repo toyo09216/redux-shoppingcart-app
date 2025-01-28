f3e60fd9f40e6b1d1117f35767ae637b9e7842cf
toyo09216 <atsushi09216@gmail.com>
2025-01-28 14:18:44 +0900

    [add] storeの中から商品情報を取り出して出力

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,24 @@
-import { CartIcon } from "../HeroIcons";
+import { MinusIcon, PlusIcon } from "../HeroIcons";
 
-export const CartItem = () => {
+export const CartItem = ({id, img, title, price, amount}) => {
   return (
-    <div>
-      cartItems
-    </div>
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">{price}円</h4>
+        <button className="remove-btn">削除</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <PlusIcon />
+        </button>
+        <p className="amount">{amount}</p>
+        <button className="amount-btn">
+          <MinusIcon />
+        </button>
+      </div>
+    </article>
   )
 }
 
