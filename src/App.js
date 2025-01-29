7805473adf27f47f9abe78d535efb56e0c71d9f2
toyo09216 <atsushi09216@gmail.com>
2025-01-29 13:37:04 +0900

    [add] モーダル用のコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CartContainer from './components/CartContainer';
 import { useEffect } from 'react';
 import { calculateTotals } from './features/cart/CartSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
@@ -14,6 +15,7 @@ function App() {
   }, [cartItems]);
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
