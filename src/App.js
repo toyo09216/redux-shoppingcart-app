1c32624848a916c5fc31e636cd84b29cb227d4dc
toyo09216 <atsushi09216@gmail.com>
2025-01-29 13:53:57 +0900

    [add] 全削除を押して、モーダルを出現させる

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
